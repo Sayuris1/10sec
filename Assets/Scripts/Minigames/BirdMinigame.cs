55b13bee6bd321db6d8a59309d2cc0c35da3604b
Alp <elgunalp1@gmail.com>
2024-09-22 21:32:57 +0100

    Generic TImer

--- Assets/Scripts/Minigames/BirdMinigame.cs
+++ Assets/Scripts/Minigames/BirdMinigame.cs
@@ -2,24 +2,4 @@ using UnityEngine;
 
 public class BirdMinigame : MinigameBase
 {
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-            IsWon = true;
-    }
-
-    protected override void InitMinigame()
-    {
-        FUI.Instance.PromptTMP.text = "Press A To Win";
-    }
-
-    protected override void WonMinigame()
-    {
-        FUI.Instance.PromptTMP.text = "Win";
-    }
-    
-    protected override void LoseMinigame()
-    {
-        FUI.Instance.PromptTMP.text = "Lost";
-    }
 }
